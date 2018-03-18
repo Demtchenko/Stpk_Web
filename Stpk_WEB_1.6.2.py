86f9976db6f04d508b414e7799a0fad83d9fed86
Demtchenko <deko@myttk.ru>
2018-03-18 21:32:09 +0700

    Для Stpk 2

--- Stpk_WEB_1.6.2.py
+++ Stpk_WEB_1.6.2.py
@@ -6,7 +6,10 @@ from concurrent.futures import ThreadPoolExecutor
 
 
 def clnt(s, n):
-    conn, addr = s.accept()
+    try:
+        conn, addr = s.accept()
+    except OSError:
+        return
     print('Клиент', n, addr)
     data = b''
     while data.decode('utf-8').strip() != 'close':
@@ -17,8 +20,10 @@ def clnt(s, n):
     conn.close
     print(n, 'Закрыли соединение')
 
-def main(cnt, ipa, prt):
+
+def main(cnt, ipa, prt, tmot):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.settimeout(tmot)
         s.bind((ipa, prt))
         s.listen(cnt)
 
@@ -27,9 +32,9 @@ def main(cnt, ipa, prt):
         loop = asyncio.get_event_loop()
         loop.set_debug(True)
         for i in range(cnt):
-        #        fclnts.append(asyncio.ensure_future(loop.run_in_executor(xctr, functools.partial(clnt, s, i))))
+            # fclnts.append(asyncio.ensure_future(loop.run_in_executor(xctr, functools.partial(clnt, s, i))))
         # для совместимости с версией 3.4.3 на Степике заменим
-        fclnts.append(asyncio.async(loop.run_in_executor(xctr, functools.partial(clnt, s, i))))
+            fclnts.append(asyncio.async(loop.run_in_executor(xctr, functools.partial(clnt, s, i))))
         loop.run_until_complete(asyncio.gather(*fclnts))
         loop.close()
         s.shutdown(socket.SHUT_RDWR)
@@ -37,4 +42,4 @@ def main(cnt, ipa, prt):
 
 
 if __name__ == '__main__':
-    main(10, '0.0.0.0', 2222)
+    main(10, '0.0.0.0', 2222, 15)
